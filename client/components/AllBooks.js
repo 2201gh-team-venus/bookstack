25413be194d5799f6cf56b205d0f2053c18bf18d
Serena Chang <serena.w.chang@gmail.com>
2022-03-01 18:30:37 -0500

    feat(component): update AllBooks component add No Books Alert

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import  {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {fetchBooks} from '../store/books';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchBooks } from "../store/books";
 
 export class AllBooks extends React.Component {
-    componentDidMount(){
-        this.props.fetchBooks();
+  componentDidMount() {
+    this.props.fetchBooks();
+  }
+  render() {
+    if (this.props.books.length === 0 || !this.props.books) {
+      return <h1>No Books!</h1>;
     }
-    render(){
-        if(this.props.books !== undefined || null){
-            return(
-                <div>
-                    <h1>All Books</h1>
-                    <div>
-                        {this.props.books.map((book) => (
-                            <div className="book" key={book.id}>
-                                <img src={book.imageURL}/>
-                                <Link to={`/books/${book.id}`}>
-                                    <h3>{book.name}</h3>
-                                </Link>
-                                {/*Include Author name and genre for book*/}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )
-        }
-    }
+    return (
+      <div>
+        <h1>All Books</h1>
+        <div>
+          {this.props.books.map((book) => (
+            <div className="book" key={book.id}>
+              <img src={book.imageURL} />
+              <Link to={`/books/${book.id}`}>
+                <h3>{book.name}</h3>
+              </Link>
+              {/*Include Author name and genre for book*/}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {books: state.books};
-}
+  return { books: state.books };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchBooks: () => dispatch(fetchBooks())
-    };
+  return {
+    fetchBooks: () => dispatch(fetchBooks()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllBooks);
