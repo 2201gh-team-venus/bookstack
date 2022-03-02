23ba49725454f5db35dfe58ef838ae98919585c6
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-03-01 19:31:44 -0500

    create database models

---
+++ server/db/models/ShoppingSession.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const ShoppingSession = db.define('shopping_session', {
+  total: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+})
+
+module.exports = ShoppingSession
+
