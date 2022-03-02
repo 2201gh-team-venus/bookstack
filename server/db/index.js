09e9aa2a240f572fd224fd27cb142261567d06fa
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-03-02 13:30:10 -0500

    create model associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,41 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Author = require('./models/Author')
+const Book = require('./models/Book')
+const CartItem = require('./models/CartItem')
+const Genre = require('./models/Genre')
+const Order = require('./models/Order')
+const ShoppingSession = require('./models/ShoppingSession')
 
 //associations could go here!
+Author.hasMany(Book)
+Book.belongsTo(Author)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+Order.hasMany(CartItem)
+CartItem.belongsTo(Order)
+
+User.hasMany(ShoppingSession)
+ShoppingSession.belongsTo(User)
+
+Book.belongsToMany(Genre, { through: 'book_genre' })
+Genre.belongsToMany(Book, { through: 'book_genre' })
+
+Book.belongsToMany(User, { through: 'book_user' })
+User.belongsToMany(Book, { through: 'book_user' })
 
 module.exports = {
   db,
   models: {
     User,
+    Author,
+    Book,
+    CartItem,
+    Genre,
+    Order,
+    ShoppingSession
   },
 }
