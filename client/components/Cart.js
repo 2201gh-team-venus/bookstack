2c720cb79b36682077d0ee5c95952cf35fb810ce
Kimberley <kimberley@email.com>
2022-03-07 15:13:36 -0500

    fix(client/components): Bug fixes Localstorage 'temp' and 'token' no longer causes conflict with each other. Total updates when user changes quantity.

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -9,7 +9,7 @@ import { allBooks, _clearBooks } from '../store/cart';
 class Cart extends React.Component {
 	constructor() {
 		super();
-		this.state = { books: null, checkout: false };
+		this.state = { books: null, total: 0, checkout: false };
 
 		this.handleBooks = this.handleBooks.bind(this);
 		this.handleTotal = this.handleTotal.bind(this);
@@ -22,21 +22,29 @@ class Cart extends React.Component {
 		// prettier-ignore
 		if (this.props.user.id) {
 			this.props.allBooks(this.props.user.id);
+			/* Fix having both locastorage 'temp 'and 'token'. */
+			localStorage.removeItem('temp');
 		}
 		
 		else if (localStorage.getItem('temp')) {
 			const books = JSON.parse(localStorage.getItem('temp'));
 			this.setState({ books: books });
+			console.log(this.state.books);
 		}
 	}
 
-	componentDidUpdate(prvState) {
-		if (prvState.user.id !== this.props.user.id) {
+	componentDidUpdate(prvProps, prvState) {
+		/* If user not logged-in get localstorage 'temp' item. */
+		if (prvProps.user.id !== this.props.user.id) {
 			this.props.allBooks(this.props.user.id);
+		} else if (localStorage.getItem('temp')) {
+			localStorage.setItem('temp', JSON.stringify(this.state.books));
 		}
 
-		if (localStorage.getItem('temp')) {
-			localStorage.setItem('temp', JSON.stringify(this.state.books));
+		if (JSON.stringify(prvState.books) !== JSON.stringify(this.state.books)) {
+			this.handleTotal();
+		} else {
+			console.log('the same');
 		}
 	}
 
@@ -76,28 +84,31 @@ class Cart extends React.Component {
 	}
 
 	handleTotal() {
+		/* Method sets state, returns null */
 		const priceReducer = object => {
-			return object.reduce((prv, cur) => ({
-				price: Number(prv.price) + Number(cur.price)
-			}));
+			const price = object.reduce((prv, cur) => {
+				return Number(prv) + Number(cur.price) * Number(cur.quantity);
+			}, 0);
+			return parseFloat(price).toFixed(2);
 		};
 
 		if (this.props.cart.length > 0) {
-			const { price } = priceReducer(this.props.cart);
-			return price;
-		} else if (
-			localStorage.getItem('temp') &&
-			this.state.books !== null &&
-			this.state.books.length > 0
-		) {
-			const { price } = priceReducer(this.state.books);
-			return price;
+			const total = priceReducer(this.props.cart);
+			this.setState({ total: total });
+			return null;
+		} else if (this.state.books !== null && this.state.books.length > 0) {
+			const total = priceReducer(this.state.books);
+			console.log('current total', total);
+			this.setState({ total: total });
+			return null;
 		} else {
-			return 0;
+			this.setState({ total: 0 });
+			return null;
 		}
 	}
 
 	handleQuantity(id, type) {
+		/* Method sets state, returns undefined */
 		const prvBooks = [...this.state.books];
 
 		const changeQuantity = prvBooks.map(obj => {
@@ -110,13 +121,15 @@ class Cart extends React.Component {
 					obj.quantity -= 1;
 				}
 			}
-
+			console.log(obj, 'state books');
 			return obj;
 		});
 
 		this.setState({
 			books: changeQuantity
 		});
+
+		this.handleTotal();
 	}
 
 	handleDelete(id) {
@@ -140,7 +153,7 @@ class Cart extends React.Component {
 
 	render() {
 		return this.state.checkout ? (
-			<Checkout books={this.state.books} total={this.handleTotal()} />
+			<Checkout books={this.state.books} total={this.state.total} />
 		) : (
 			<div className="cart">
 				<h3>
@@ -150,7 +163,7 @@ class Cart extends React.Component {
 				<div className="items">{this.handleBooks()}</div>
 				<div className="cart-total">
 					<h4 className="cart-total__text">Total</h4>
-					<h4 className="cart-total__amount">${this.handleTotal()}</h4>
+					<h4 className="cart-total__amount">${this.state.total}</h4>
 				</div>
 				<div className="cart-checkout">
 					{this.props.user.id ? '' : 'Not logged in. You are ordering as guest'}
