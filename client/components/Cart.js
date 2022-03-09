616d67dc0c0d8b38571c48ddaf65c1d5efe6588e
Kimberley <kimberley@email.com>
2022-03-08 23:18:25 -0500

    fix(client/components): Cart shows updated total.

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -7,33 +7,29 @@ import Checkout from './Checkout';
 import { cartItems, _clearBooks, addBook, removeBook, editQuantity } from '../store/cart';
 
 class Cart extends React.Component {
-<<<<<<< HEAD
 	constructor() {
 		super();
-		this.state = { books: null, total: 0, checkout: false };
-
+		this.state = { total: 0, checkout: false };
 		this.handleBooks = this.handleBooks.bind(this);
 		this.handleQuantity = this.handleQuantity.bind(this);
-
 		this.handleTotal = this.handleTotal.bind(this);
-
-		// this.handleDelete = this.handleDelete.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
 		// this.handleCheckout = this.handleCheckout.bind(this);
 	}
-
 	componentDidMount() {
 		if (localStorage.getItem('token')) {
 			this.props.cartItems();
-			this.handleTotal();
 		}
-	}
 
+		this.props.cart.forEach(elm => {
+			console.log(elm);
+		});
+
+		this.handleTotal();
+	}
 	componentDidUpdate(prvProps, prvState) {
-		if (prvProps.user.id !== this.props.user.id) {
-			if (prvState.total !== this.state.total) {
-				this.handleTotal();
-			}
-			this.props.cartItems();
+		if (prvProps.cart.length < 0) {
+			this.handleTotal();
 		}
 	}
 
@@ -42,7 +38,6 @@ class Cart extends React.Component {
 			this.props.clearBooks();
 		}
 	}
-
 	handleBooks() {
 		return this.props.cart.map(obj => (
 			<CartItems
@@ -54,43 +49,34 @@ class Cart extends React.Component {
 			/>
 		));
 	}
-
 	handleTotal() {
-		/* Method sets state, returns null */
-		const priceReducer = books => {
-			const price = books.reduce((prv, cur) => {
-				console.log(prv);
-				console.log(cur);
-
-				return Number(prv) + Number(cur.price) * Number(cur.quantity);
+		/* TOFIX: state not updating price even using componentDidUpdate */
+		setTimeout(() => {
+			const total = this.props.cart.reduce((previous, current) => {
+				console.log('current price', current.book.price);
+				return (
+					Number(previous) +
+					Number(current.book.price) * Number(current.quantity)
+				);
 			}, 0);
 
-			return parseFloat(price).toFixed(2);
-		};
-
-		if (this.props.cart.length > 0) {
-			const total = priceReducer(this.props.cart);
-			console.log(total);
-			console.log(this.props.cart);
 			this.setState({ total: total });
-			return null;
-		}
+		}, 500);
 	}
 
 	handleQuantity(quantity, type, book) {
 		let editQuantity = 0;
+		console.log('Current', quantity);
 		if (type === 'increase') {
 			editQuantity = quantity + 1;
 		} else if (type === 'decrease') {
 			editQuantity = quantity - 1;
 		}
-
 		this.props.editQuantity(book, editQuantity);
-		// this.handleTotal();
 	}
 
 	handleDelete(book) {
-		this.props.removeBook(book);
+		this.props.deleteBook(book);
 	}
 
 	handleCheckout() {
@@ -98,7 +84,6 @@ class Cart extends React.Component {
 	}
 
 	render() {
-		console.log(this.props);
 		return this.state.checkout ? (
 			<Checkout books={this.state.books} total={this.state.total} />
 		) : (
@@ -108,7 +93,6 @@ class Cart extends React.Component {
 					books
 				</h3>
 				<div className="items">{this.handleBooks()}</div>
-
 				<div className="cart-total">
 					<h4 className="cart-total__text">Total</h4>
 					<h4 className="cart-total__amount">${this.state.total}</h4>
@@ -117,7 +101,7 @@ class Cart extends React.Component {
 					{this.props.user.id ? '' : 'Not logged in. You are ordering as guest'}
 					<button
 						disabled={
-							this.state.books === null || this.state.books.length === 0
+							this.props.cart === null || this.props.cart.length === 0
 								? true
 								: false
 						}
@@ -130,139 +114,22 @@ class Cart extends React.Component {
 			</div>
 		);
 	}
-=======
-    constructor() {
-        super();
-        this.state = { books: null, total: 0, checkout: false };
-        this.handleBooks = this.handleBooks.bind(this);
-        this.handleQuantity = this.handleQuantity.bind(this);
-        this.handleTotal = this.handleTotal.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        // this.handleCheckout = this.handleCheckout.bind(this);
-    }
-    componentDidMount() {
-        if (localStorage.getItem('token')) {
-            this.props.cartItems();
-            this.handleTotal();
-        }
-    }
-    componentDidUpdate(prvProps, prvState) {
-        if (prvProps.user.id !== this.props.user.id) {
-            if (prvState.total !== this.state.total) {
-                this.handleTotal();
-            }
-            this.props.cartItems();
-        }
-    }
-    componentWillUnmount() {
-        if (this.props.user.id) {
-            this.props.clearBooks();
-        }
-    }
-    handleBooks() {
-        return this.props.cart.map(obj => (
-            <CartItems
-                key={obj.id}
-                data={obj.book}
-                quantity={obj.quantity}
-                quantityFn={this.handleQuantity}
-                removeBookFn={this.handleDelete}
-            />
-        ));
-    }
-    handleTotal() {
-        /* Method sets state, returns null */
-        const priceReducer = books => {
-            const price = books.reduce((prv, cur) => {
-                console.log(prv);
-                console.log(cur);
-                return Number(prv) + Number(cur.price) * Number(cur.quantity);
-            }, 0);
-            return parseFloat(price).toFixed(2);
-        };
-        if (this.props.cart.length > 0) {
-            const total = priceReducer(this.props.cart);
-            console.log(total);
-            console.log(this.props.cart);
-            this.setState({ total: total });
-            return null;
-        }
-    }
-    handleQuantity(quantity, type, book) {
-        let editQuantity = 0;
-        if (type === 'increase') {
-            editQuantity = quantity + 1;
-        } else if (type === 'decrease') {
-            editQuantity = quantity - 1;
-        }
-        this.props.editQuantity(book, editQuantity);
-        // this.handleTotal();
-		// this.props.cartItems();
-    }
-    handleDelete(book) {
-        this.props.deleteBook(book);
-    }
-    handleCheckout() {
-        this.setState({ checkout: true });
-    }
-    render() {
-        console.log(this.props);
-        return this.state.checkout ? (
-            <Checkout books={this.state.books} total={this.state.total} />
-        ) : (
-            <div className="cart">
-                <h3>
-                    {this.props.user.username ? this.props.user.username : 'Visitor'} 's
-                    books
-                </h3>
-                <div className="items">{this.handleBooks()}</div>
-                <div className="cart-total">
-                    <h4 className="cart-total__text">Total</h4>
-                    <h4 className="cart-total__amount">${this.state.total}</h4>
-                </div>
-                <div className="cart-checkout">
-                    {this.props.user.id ? '' : 'Not logged in. You are ordering as guest'}
-                    <button
-                        disabled={
-                            this.state.books === null || this.state.books.length === 0
-                                ? true
-                                : false
-                        }
-                        onClick={this.handleCheckout}
-                        className="cart-checkout__btn"
-                        type="button">
-                        Checkout
-                    </button>
-                </div>
-            </div>
-        );
-    }
->>>>>>> main
 }
 
 const mapState = state => {
-    return {
-        user: state.auth,
-        cart: state.cart
-    };
+	return {
+		user: state.auth,
+		cart: state.cart
+	};
 };
 
 const mapDispatch = dispatch => {
-<<<<<<< HEAD
 	return {
 		cartItems: () => dispatch(cartItems()),
 		clearBooks: () => dispatch(_clearBooks()),
-		// removeBook: book => dispatch(deleteBook(book)),
+		deleteBook: book => dispatch(removeBook(book)),
 		editQuantity: (book, quantity) => dispatch(editQuantity(book, quantity))
 	};
-=======
-    return {
-        cartItems: () => dispatch(cartItems()),
-        clearBooks: () => dispatch(_clearBooks()),
-        deleteBook: book => dispatch(removeBook(book)),
-        editQuantity: (book, quantity) => dispatch(editQuantity(book, quantity))
-    };
->>>>>>> main
 };
 
 export default connect(mapState, mapDispatch)(Cart);
